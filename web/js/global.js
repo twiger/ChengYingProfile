d63203d1b01bb4fcb070b810982b36ef35f0226b
ZhengYing <promisemyself1215@gmail.com>
2018-11-05 15:43:12 +0800

    more respomnsive

--- web/js/global.js
+++ web/js/global.js
@@ -1,10 +1,7 @@
+(function($) {
+    $(function() {
 
-(function($){
-  $(function(){
-
-    
-
-  }); // end of document ready
+    }); // end of document ready
 })(jQuery); // end of jQuery name space
 
 
@@ -24,4 +21,20 @@ function getIP() {
     console.log("Get IP!");
     // return 'http://140.116.72.187:5000';
     return 'http://127.0.0.1:5000';
+}
+
+var sideNavClose = true;
+
+function triggerSideNav() {
+
+    if (sideNavClose) {
+        $('.side-nav').css('margin', '0px 315px');
+        document.getElementsByTagName("main")[0].style.marginLeft = "300px";
+        sideNavClose = false;
+    }
+    else{
+        $('.side-nav').css('margin', '0px 0px');
+        document.getElementsByTagName("main")[0].style.marginLeft = "";
+        sideNavClose = true;
+    }
 }
---
+++ web/js/hwReviewCtrl.js
@@ -0,0 +1,11 @@
+var app = angular.module('colors', []);
+
+app.controller('hwReviewCtrl', function($scope, $http) {
+
+    $('.tabs').tabs();
+
+    // $("#triger-nav").click(function() {
+    //     console.log("click");
+    // });
+
+});
